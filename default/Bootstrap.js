3eebf05857dd8388d219c31aab2118ada11f3ca2
godlikemouse <godlikemouse@godlikemouse.com>
2016-07-07 16:04:04 -0700

    Added bootstrap file functionality.

---
+++ default/Bootstrap.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class Bootstrap {
+
+		//all methods that start with init will be executed when
+		//the Zenhance server starts
+		//
+		//initDoSomething() { 
+		//    Do something useful...
+		//}
+
+}
